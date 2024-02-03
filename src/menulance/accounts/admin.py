4700f29530bfaf1889182cf4c2dda4fde32eaeaa
hassanahmed <ahmed.hassan.112.ha@gmail.com>
2024-02-04 04:35:30 +0500

    feat(accounts): add user model to django admin

--- src/menulance/accounts/admin.py
+++ src/menulance/accounts/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+from accounts.models import UserPreferences
+
+
+class UserPreferencesAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserPreferences, UserPreferencesAdmin)
